670ac55ce5a486d6c6c8376607e993df027ada79
Basset <aldana.sorni@gmail.com>
2022-01-11 21:43:04 -0300

    correción de errores y adición de ventanas editar y eliminar

--- js/admin.js
+++ js/admin.js
@@ -1,6 +1,6 @@
 // ------------------------------ E L E M E N T O S ------------------------------
 // ------------------------------ MODAL NUEVA VENTA ------------------------------
-const ventanaModal = document.getElementById('ocultar-modal');
+const ventanaModalVenta = document.getElementById('ocultar-modal-nueva-venta');
 const nuevaVenta = document.getElementById('nueva-venta');
 const listaVendedoras = document.getElementById('vendedoras');
 const listaComponentes = document.getElementById('componentes');
@@ -8,6 +8,20 @@ const listaSucursales = document.getElementById('sucursales');
 const aceptarNuevaVenta = document.getElementById('nueva-venta-aceptar');
 const cancelarNuevaVenta = document.getElementById('nueva-venta-cancelar');
 
+// ------------------------------ MODAL EDITAR VENTA ------------------------------
+const ventanaModalEditar = document.getElementById('ocultar-modal-editar-venta');
+const btnEditar = document.querySelector('.boton-editar');
+const listaVendedorasEdit = document.getElementById('vendedoras-edit');
+const listaComponentesEdit = document.getElementById('componentes-edit');
+const listaSucursalesEdit = document.getElementById('sucursales-edit');
+const aceptarEditarVenta = document.getElementById('editar-venta-aceptar');
+const cancelarEditarVenta = document.getElementById('editar-venta-cancelar');
+
+// ------------------------------ MODAL ELIMINAR VENTA ------------------------------
+const ventanaModalEliminar = document.getElementById('ocultar-modal-eliminar-venta');
+const btnEliminar = document.querySelector('.boton-eliminar');
+const aceptarEliminarVenta = document.getElementById('eliminar-venta-aceptar');
+const cancelarEliminarVenta = document.getElementById('eliminar-venta-cancelar');
 
 // ------------------------------ A R R A Y S ------------------------------
 const vendedoras = ["Ada", "Grace", "Hedy", "Sheryl"];
@@ -20,49 +34,105 @@ const sucursales = ["Centro", "Caballito"];
 
 // ------------------------------ L O G I C A ------------------------------
 
-// ------------------------------ Desplegables del form de la modal ------------------------------
+// ------------------------------ Desplegables del form de la modal Nueva Venta ------------------------------
 
-const sumarVendedoras = (e) => {
+
+const sumarVendedoras = () => {
     for (let i=0; i < vendedoras.length; i++){
         const optionVendedora = document.createElement('option');
         listaVendedoras.appendChild(optionVendedora);
         optionVendedora.innerText = `${vendedoras[i]}`;
     }
-    return vendedoras;
-    }
+}
+sumarVendedoras();
     
-const sumarComponentes = (e) => {
-        for (let i=0; i < precios.length; i++){
-            const optionComponente = document.createElement('option');
-            optionComponente.setAttribute('id', 'seleccion-componente');
-            listaComponentes.appendChild(optionComponente);
-            optionComponente.innerText = `${precios[i][0]}`;
+const sumarComponentes = () => {
+    for (let i = 0; i < precios.length; i++) {
+        const optionComponente = document.createElement('option');
+        optionComponente.setAttribute('id', 'seleccion-componente');
+        listaComponentes.appendChild(optionComponente);
+        optionComponente.innerText = `${precios[i][0]}`;
     }
-    return precios;
-    }
+}
+sumarComponentes();
 
-    const sumarSucursales = (e) => {
-        for (let i=0; i < sucursales.length; i++){
+const sumarSucursales = () => {
+    for (let i=0; i < sucursales.length; i++){
             const optionSucursal = document.createElement('option');
             listaSucursales.appendChild(optionSucursal);
             optionSucursal.innerText = `${sucursales[i]}`;
-        }
-        return sucursales;
-        }
+    }
+}
+sumarSucursales();
+
+// ------------------------------ Desplegables del form de la modal Editar Venta ------------------------------
+
+
+const sumarVendedorasEdit = () => {
+    for (let i=0; i < vendedoras.length; i++){
+        const optionVendedora = document.createElement('option');
+        listaVendedorasEdit.appendChild(optionVendedora);
+        optionVendedora.innerText = `${vendedoras[i]}`;
+    }
+}
+sumarVendedorasEdit();
+    
+const sumarComponentesEdit = () => {
+    for (let i = 0; i < precios.length; i++) {
+        const optionComponente = document.createElement('option');
+        optionComponente.setAttribute('id', 'seleccion-componente');
+        listaComponentesEdit.appendChild(optionComponente);
+        optionComponente.innerText = `${precios[i][0]}`;
+    }
+}
+sumarComponentesEdit();
+
+const sumarSucursalesEdit = () => {
+    for (let i=0; i < sucursales.length; i++){
+            const optionSucursal = document.createElement('option');
+            listaSucursalesEdit.appendChild(optionSucursal);
+            optionSucursal.innerText = `${sucursales[i]}`;
+    }
+}
+sumarSucursalesEdit();
 
 // ------------------------------ Apertura y cierre de ventanas modal ------------------------------
-nuevaVenta.addEventListener('click', (e) => {
-    ventanaModal.classList.remove('ocultar-modal');
-    sumarVendedoras();
-    sumarComponentes();
-    sumarSucursales();
+
+//NUEVA VENTA
+nuevaVenta.addEventListener('click', () => {
+    ventanaModalVenta.classList.remove('ocultar-modal');
 })
 
 aceptarNuevaVenta.addEventListener('click', () => {
-    ventanaModal.classList.add('ocultar-modal');
+    ventanaModalVenta.classList.add('ocultar-modal');
 })
 
 cancelarNuevaVenta.addEventListener('click', () => {
-    ventanaModal.classList.add('ocultar-modal');
+    ventanaModalVenta.classList.add('ocultar-modal');
 })
 
+//EDITAR VENTA
+btnEditar.addEventListener('click', () => {
+    ventanaModalEditar.classList.remove('ocultar-modal');
+})
+
+aceptarEditarVenta.addEventListener('click', () => {
+    ventanaModalEditar.classList.add('ocultar-modal');
+})
+
+cancelarEditarVenta.addEventListener('click', () => {
+    ventanaModalEditar.classList.add('ocultar-modal');
+})
+
+//ELIMINAR VENTA 
+btnEliminar.addEventListener('click', () => {
+    ventanaModalEliminar.classList.remove('ocultar-modal');
+})
+
+aceptarEliminarVenta.addEventListener('click', () => {
+    ventanaModalEliminar.classList.add('ocultar-modal');
+})
+
+cancelarEliminarVenta.addEventListener('click', () => {
+    ventanaModalEliminar.classList.add('ocultar-modal');
+})
